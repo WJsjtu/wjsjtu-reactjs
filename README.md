9d0c02dd0c1fee70d45cd55d3ad8bef22db519ce
Jason Wang <JasonWang0421@gmail.com>
2015-02-03 18:35:05 +0800

    import files

---
+++ README.md
@@ -0,0 +1,59 @@
+# wjsjtu-reactjs  v0.0.1 
+## Getting Started
+```shell
+npm install wjsjtu-reactjs --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('wjsjtu-reactjs ');
+```
+
+### Options
+
+#### wrapper
+Type: `Boolean` 
+Default: `false`
+
+Turn on or off wrapper with default options. If an `wrapper` is specified, it uses `replaceWord` to replace `React.createElement` compiled code, for it often appears in compiled code, this will make the size of minified files smaller.
+
+#### replaceWord
+Type: `String`
+Default: `"__ReactCreateElement"`
+
+It will generate `"(function(React, window){var "` + `replaceWord` + `" = React.createElement;" + result + "})(React, window);"` ,which wraps the code with closure to make it works better when using minify tools.
+
+### Usage examples
+
+#### Basic compression
+
+This configuration will just use `react-tools` to transfer files using the default options.
+
+```js
+// Project configuration.
+grunt.initConfig({
+  reactjs: [{
+    src: ['src/input1.jsx', 'src/input2.jsx'],
+    dest: 'dest/output.min.js'
+  }]
+});
+```
+
+
+Specify `wrapper: false` to wrap the compiled code.
+
+```js
+// Project configuration.
+grunt.initConfig({
+  reactjs: {
+     main:{
+      files: [{
+        wrapper: true,
+        src:"src/jsx/datepicker.jsx",
+        dest: "dist/js/datepicker.js"
+      }]
+    }
+ }
+});
+```
---
+++ tasks/reactjs.js
@@ -0,0 +1,92 @@
+/*
+ * wjsjtu-reactjs
+ *
+ * Copyright (c) 2014 Jason Wang, contributors
+ * Licensed under the MIT license.
+ * https://github.com/WJsjtu/wjsjtu-reactjs/blob/master/LICENSE-MIT
+ */
+
+module.exports = function(grunt) {
+	'use strict';
+
+	var path = require('path');
+	var fs = require('fs');
+	var chalk = require('chalk');
+	var crypto = require('crypto');
+	var reactTools = require('react-tools');
+	grunt.registerMultiTask('reactjs', 'Transfer reactjs JSX files.',
+	function() {
+
+		var options = this.options({
+			encoding: grunt.file.defaultEncoding,
+			wrapper: false,
+			replaceWord: "__ReactCreateElement",
+		});
+
+		var writeOptions = {
+			encoding: options.encoding
+		};
+
+		var dest;
+		var isExpandedPair;
+		var tally = {
+			dirs: 0,
+			files: 0,
+		};
+
+		this.files.forEach(function(filePair) {
+			isExpandedPair = filePair.orig.expand || false;
+			filePair.src.forEach(function(src) {
+				if (detectDestType(filePair.dest) === 'directory') {
+					dest = (isExpandedPair) ? filePair.dest: unixifyPath(path.join(filePair.dest, src));
+				} else {
+					dest = filePair.dest;
+				}
+				if (grunt.file.isDir(src)) {
+					grunt.verbose.writeln('Creating ' + chalk.cyan(dest));
+					grunt.file.mkdir(dest);
+					tally.dirs++;
+				} else {
+					grunt.verbose.writeln('Transferring jsx ' + chalk.cyan(src) + ' -> ' + chalk.cyan(dest));
+					var result = grunt.file.read(src, writeOptions);
+					if (result) {
+						result = reactTools.transform(result, {});
+						if (options.wrapper) {
+
+							result.replace("React.createElement", options.replaceWord);
+							result = "(function(React, window){var " + options.replaceWord + " = React.createElement;" + result + "})(React, window);";
+						}
+						grunt.file.write(dest, result, writeOptions);
+					}
+					tally.files++;
+				}
+			});
+		});
+
+		if (tally.dirs) {
+			grunt.log.write('Created ' + chalk.cyan(tally.dirs.toString()) + ' directories');
+		}
+
+		if (tally.files) {
+			grunt.log.write((tally.dirs ? ', transferred ': 'Transferred ') + chalk.cyan(tally.files.toString()) + (tally.files === 1 ? ' file': ' files'));
+		}
+
+		grunt.log.writeln();
+	});
+
+	var detectDestType = function(dest) {
+		if (grunt.util._.endsWith(dest, '/')) {
+			return 'directory';
+		} else {
+			return 'file';
+		}
+	};
+
+	var unixifyPath = function(filepath) {
+		if (process.platform === 'win32') {
+			return filepath.replace(/\\/g, '/');
+		} else {
+			return filepath;
+		}
+	};
+};
